72f9bfb4ed62394431f86130ba70b1576c4ed458
meazer <meazer.m@gmail.com>
2018-12-27 17:00:04 +0100

    create new post page

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const expressEdge = require('express-edge');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/node-js-blog');
 const app = express();
 
 app.use(express.static('public'));
@@ -10,6 +12,10 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.render('index');
+})
+
+app.get('/posts/new', (req, res) => {
+  res.render('create');
 })
 
 app.get('/about', (req, res) => {
