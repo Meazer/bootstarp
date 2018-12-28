500c21c40367f6a17adf7cf06dc11a7fa600663c
meazer <meazer.m@gmail.com>
2018-12-28 12:01:07 +0100

    read posts from db

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await post.find({});
+  res.render('index', { posts });
 })
 
 app.get('/posts/new', (req, res) => {
