355f8d5f73b2dfcd076a217767010b2be1c235cf
meazer <meazer.m@gmail.com>
2019-01-03 19:51:57 +0100

    save encrypted password

--- database/models/User.js
+++ database/models/User.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: String,
   email: String,
   password: String
+});
+
+userSchema.pre('save', function (next) {
+  const user = this;
+  bcrypt.hash(user.password, 10, (error, encrypted) => {
+    user.password = encrypted;
+    next();
+  })
 });
 
 module.exports = new mongoose.model('User', userSchema);
