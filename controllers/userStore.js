41ccbe3cd0f6469e3f500e1fcc5394c92e0a84f7
meazer <meazer.m@gmail.com>
2019-01-07 12:54:40 +0100

    persist request data on form

--- controllers/userStore.js
+++ controllers/userStore.js
@@ -5,6 +5,7 @@ module.exports = (req, res) => {
   user.create(req.body, (error, user) => {
     if (error) {
       req.flash('registrationErrors', Object.keys(error.errors).map(key => error.errors[key].message));
+      req.flash('data', req.body);
       return res.redirect('/auth/register');
     }
     res.redirect('/')
